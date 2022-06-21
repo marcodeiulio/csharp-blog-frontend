30e9fe26da5b5b3ecf522f7240ceff30cdba27c0
marcodeiulio <mdeiulio@icloud.com>
2022-06-21 10:31:44 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using csharp_blog_frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace csharp_blog_frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(id);
+        }
+
+        [HttpGet]
+        public IActionResult CreateFile()
+        {
+            return View();
+        }
+
+        //[HttpGet]
+        //public IActionResult CreateConBlob(int id)
+        //{
+        //    return View(id);
+        //}
+
+
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(id);
+        }
+    }
+}
